2710d0fcdf21f24ff37077ed99e5246f67203d4a
VineethSingh <87272178+VineethSingh@users.noreply.github.com>
2021-07-27 21:00:36 +0530

    Create task.py

---
+++ task.py
@@ -0,0 +1,10 @@
+PI = 3.142
+r = float(input(' Input the radius of the circle: '))
+area = PI * r * r
+print(" The area Of the Circle with radius'+r' is ' = %f" %area)
+
+
+
+fn= input("Input the Filename: ")
+f = fn.split(".")
+print ("The extension of the file is : " + f[-1])
